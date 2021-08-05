b8fb3cc6fb584272ae42a32977fa8af71d149282
Pierre-Louis Sergent <pierrelouis.sergent@orange.com>
2021-08-05 17:11:08 +0200

    Upload local file on gdrive and retrieve remote file on startup + refactor code with google tools

---
+++ google_auth.py
@@ -0,0 +1,43 @@
+from __future__ import print_function
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import Flow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+
+class GoogleAuth:
+    def __init__(self):
+        self.scopes =  ['https://www.googleapis.com/auth/drive']
+        self.creds = None
+        self.service = self.authenticate()
+        self.flow = Flow.from_client_secrets_file(
+                    'credentials.json', self.scopes, redirect_uri='urn:ietf:wg:oauth:2.0:oob')
+
+    def authenticate(self):
+        if os.path.exists('token.json'):
+            self.creds = Credentials.from_authorized_user_file('token.json', self.scopes)
+        # If there are no (valid) credentials available, let the user log in.
+        if not self.creds or not self.creds.valid:
+            if self.creds and self.creds.expired and self.creds.refresh_token:
+                self.creds.refresh(Request())
+            else:
+                
+                # Tell the user to go to the authorization URL.
+                auth_url, _ = self.flow.authorization_url(prompt='consent')
+
+                print('Please go to this URL: {}'.format(auth_url))
+
+                # The user will get an authorization code. This code is used to get the
+                # access token.
+                code = input('Enter the authorization code: ')
+                self.flow.fetch_token(code=code)
+                self.creds = self.flow.credentials
+
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(self.creds.to_json())
+
+        print('Authentication done!')
+
+        return build('drive', 'v3', credentials=self.creds)
---
+++ osu_automate.py
@@ -0,0 +1,96 @@
+from google_auth import GoogleAuth
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload
+
+import time
+import ntpath
+import os
+import io
+
+
+class OsuHandler(PatternMatchingEventHandler):
+    def __init__(self, pattern, service):
+        super().__init__(
+            patterns=pattern,
+            ignore_patterns=None,
+            ignore_directories=False,
+            case_sensitive=False
+        )
+        self.gdrive_service = service
+        print("Start watching for osu! files...")
+
+    def on_created(self, event):
+        print(f"hey, {event.src_path} has been created!")
+        # os.startfile(event.src_path) > TODO: open file automatically
+        self.send_song_to_drive(event.src_path)
+
+    def on_moved(self, event):
+        print(f"hey, {event.src_path} has been moved to {event.dest_path}!")
+        # os.startfile(event.dest_path) > TODO: open file automatically
+        self.send_song_to_drive(event.dest_path)
+
+    def send_song_to_drive(self, path):   
+        metadata = {
+            "name": ntpath.basename(path),
+            "parents": ["1TibAGp-Y_9AWQKfZoWW3zbx0yNFHidJC"] # Parent folder id
+        }
+
+        media = MediaFileUpload(path, mimetype='application/octet-stream')
+        self.gdrive_service.files().create(body=metadata,
+                                    media_body=media,
+                                    fields='id').execute()
+
+    
+class StartupCheck:
+    def __init__(self, service):
+        self.gdrive_service = service
+
+    def check_songs_on_startup(self):
+        response_data = self.gdrive_service.files().list(q="parents='1TibAGp-Y_9AWQKfZoWW3zbx0yNFHidJC'").execute()
+
+        list_remote = [(data['id'], data['name']) for data in response_data['files']] # Get song id
+        list_local  = [file.split(" ")[0] for file in os.listdir("/home/psergent/osu/")] # TODO : pass to args
+
+        # remote[0] = id, remote[1] = filename
+        for remote in list_remote:
+            if remote[1].split(" ")[0] not in list_local:
+                script_path = os.getcwd()
+                os.chdir("/home/psergent/osu/")
+                
+                file_id = remote[0]
+                request = self.gdrive_service.files().get_media(fileId=file_id)
+                fh = io.FileIO(remote[1], 'wb')
+                print(f"Downloading {remote[1]}...")
+                MediaIoBaseDownload(fh, request)
+                print("Done")
+
+                os.chdir(script_path)
+
+if __name__ == "__main__":
+    
+    folder = "/home/psergent/Downloads/" # TODO : pass to args
+    pattern = ["*.osk", "*.osz"]
+    google_authenticator = GoogleAuth()
+    gdrive_service = google_authenticator.service
+
+    startup = StartupCheck(gdrive_service).check_songs_on_startup()
+
+    event_handler = OsuHandler(pattern, gdrive_service)
+    observer = Observer()
+    observer.schedule(event_handler, folder, recursive=True)
+
+    observer.start()
+
+    try:
+        timer = 0
+        while True:
+            time.sleep(1)
+            timer = timer + 1
+
+            if timer > 3600:
+                
+                timer = 0
+    except KeyboardInterrupt:
+        observer.stop()
+        observer.join()
